fc9b2d4d1bc1499ee5e92f8864c4d918a3ce8630
SajaRaghebHamedS4A <raghebsaja527@gmail.com>
2023-03-27 20:41:30 +0300

    add function

--- Operations.py
+++ Operations.py
@@ -7,6 +7,11 @@ class opeartion:
         return x + y  # This function adds two numbers
     def add(x, y):
         return x + y
+    
+    # This function adds two numbers
+    def add(x, y):
+        return x + y  # This function adds two numbers
+    def add(x, y):
 
     # This function multiplies two numbers
     def multiply(x, y):
