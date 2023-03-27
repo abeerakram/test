b4375d4d0bee1471633229a12a0cd851ce4a7f47
AbeerAkramS4A <abeerakram978@gamil.com>
2023-03-28 01:14:49 +0300

    add file test.py

--- Calculator.py
+++ Calculator.py
@@ -152,19 +152,21 @@ if choice == ('1'):
         print("2.Subtract")
         print("3.Multiply")
         print("4.Divide")
-        choice = input("Enter choice(1/2/3/4): ")if choice == ('1'):
+        choice = input("Enter choice(1/2/3/4): ")
+        
         print("Select operation.")
         print("1.Add")
         print("2.Subtract")
         print("3.Multiply")
         print("4.Divide")
-        choice = input("Enter choice(1/2/3/4): ")vif choice == ('1'):
+        choice = input("Enter choice(1/2/3/4): ")
+        
         print("Select operation.")
         print("1.Add")
         print("2.Subtract")
         print("3.Multiply")
         print("4.Divide")
-        choice = input("Enter choice(1/2/3/4): ")if choice == ('1'):
+        choice = input("Enter choice(1/2/3/4): ")
         print("Select operation.")
         print("1.Add")
         print("2.Subtract")
