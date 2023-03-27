0d3392b72cc6965b56ae70446989cb1884947f05
AbeerAkramS4A <abeerakram978@gamil.com>
2023-03-28 01:16:26 +0300

    On branch bbbb  Your branch is up to date with 'origin/bbbb'.
     Changes to be committed:
    	new file:   test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+if choice == ('1'):
+        print("Select operation.")
+        print("1.Add")
+        print("2.Subtract")
+        print("3.Multiply")
+        print("4.Divide")
+        choice = input("Enter choice(1/2/3/4): ")
+        
