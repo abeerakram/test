1f53c2a83d5f57095a92e9c35e56cbd22911cf99
SajaRaghebHamedS4A <raghebsaja527@gmail.com>
2023-03-28 10:10:57 +0300

    add function

--- Operations.py
+++ Operations.py
@@ -281,23 +281,7 @@ if choice == ('1'):
         print("3.Multiply")
         print("4.Divide")
         choice = input("Enter choice(1/2/3/4): ")if choice == ('1'):
-        print("Select operation.")
-        print("1.Add")
-        print("2.Subtract")
-        print("3.Multiply")
-        print("4.Divide")
-        choice = input("Enter choice(1/2/3/4): ")vif choice == ('1'):
-        print("Select operation.")
-        print("1.Add")
-        print("2.Subtract")
-        print("3.Multiply")
-        print("4.Divide")
-        choice = input("Enter choice(1/2/3/4): ")if choice == ('1'):
-        print("Select operation.")
-        print("1.Add")
-        print("2.Subtract")
-        print("3.Multiply")
-        print("4.Divide")
+        
         choice = input("Enter choice(1/2/3/4): ")
         print("Invalid Input")
 if choice == ('1'):
